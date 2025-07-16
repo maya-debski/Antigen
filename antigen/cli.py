47aee0fc6faf9b9cdf2c15125c022f0988fd8de8
Jason Vestuto <jason.vestuto@austin.utexas.edu>
2025-07-16 17:54:26 -0500

    [cli][utils] Refactor, relocate argparse related methods to cli module, cleanup docstrings

--- antigen/cli.py
+++ antigen/cli.py
@@ -1,11 +1,14 @@
-#!/usr/bin/env python
-
-import argparse as ap
+from argparse import ArgumentParser
 import datetime
 import os
+import sys
+from datetime import datetime as dt
 
 
 def get_args():
+    """
+    Purpose: Input arg handling for VIRUS2 data reduction CLI tools, e.g. antigen_reduce_virus2.py
+    """
 
     defaults = {
         'infolder' : os.curdir,
@@ -33,7 +36,7 @@ def get_args():
         'twilight_flat_label': 'The object name from the FITS header card for twilight flat files, (default: %(default)s)',
     }
 
-    parser = ap.ArgumentParser(add_help=True)
+    parser = ArgumentParser(add_help=True)
 
     parser.add_argument('-i', '--infolder', type=str, help=helps['infolder'], default=defaults['infolder'])
     parser.add_argument('-o', '--outfolder', type=str, help=helps['outfolder'], default=defaults['outfolder'])
@@ -50,3 +53,113 @@ def get_args():
 
     return args
 
+
+def setup_parser():
+    ''' BRIEF DESCRIPTION '''
+    # TODO: update docstring
+    # TODO: specify what CLI tool or script this is used for?
+    parser = ArgumentParser(add_help=True)
+
+    parser.add_argument("-sd", "--start_date",
+                        help='''Start Date, e.g., 20170321, YYYYMMDD''',
+                        type=str, default=None)
+
+    parser.add_argument("-ed", "--end_date",
+                        help='''Start Date, e.g., 20170326, YYYYMMDD''',
+                        type=str, default=None)
+
+    parser.add_argument("-dl", "--date_length",
+                        help='''Days after/before start/end date, e.g., 10''',
+                        type=int, default=None)
+
+    parser.add_argument("-r", "--rootdir",
+                        help='''Root Directory for Date''',
+                        type=str, default='/work/03946/hetdex/maverick')
+
+    parser.add_argument("-in", "--instrument",
+                        help='''Instrument, e.g., virus''',
+                        type=str, default='virus')
+
+    return parser
+
+
+def setup_basic_parser():
+    ''' BRIEF DESCRIPTION '''
+    # TODO: update docstring
+    # TODO: specify what CLI tool or script this is used for?
+    parser = ArgumentParser(add_help=True)
+
+    parser.add_argument("-d", "--date",
+                        help='''Date, e.g., 20170321, YYYYMMDD''',
+                        type=str, default=None)
+
+    parser.add_argument("-o", "--observation",
+                        help='''Observation number, "00000007" or "7"''',
+                        type=str, default=None)
+
+    parser.add_argument("-e", "--exposure_number",
+                        help='''Exposure number, 10''',
+                        type=int, default=None)
+
+    parser.add_argument("-r", "--rootdir",
+                        help='''Root Directory for Reductions''',
+                        type=str, default='/work/03946/hetdex/maverick')
+
+    parser.add_argument("-in", "--instrument",
+                        help='''Instrument, e.g., lrs2''',
+                        type=str, default='lrs2')
+
+    parser.add_argument("-i", "--ifuslot",
+                        help='''Ifuslot, e.g., 066''',
+                        type=str, default='066')
+
+    parser.add_argument("-s", "--side",
+                        help='''Instrument Side, e.g., L''',
+                        type=str, default='L')
+
+    return parser
+
+
+def set_daterange(args):
+    # TODO: add docstring
+    # TODO: specify what CLI tool or script this is used for? location here is based on input/output of Argparser object
+    dateatt = ['start_date', 'end_date']
+    if args.date_length is None:
+        if args.start_date is None:
+            args.log.error('You must include two of the following: '
+                           '"start_date", "end_date", or "date_length"')
+            sys.exit(1)
+        if args.end_date is None:
+            args.log.error('You must include two of the following: '
+                           '"start_date", "end_date", or "date_length"')
+            sys.exit(1)
+        dates = {}
+        for da in dateatt:
+            dates[da] = dt(int(getattr(args, da)[:4]),
+                           int(getattr(args, da)[4:6]),
+                           int(getattr(args, da)[6:]))
+
+        args.daterange = [datetime.date.fromordinal(i)
+                          for i in range(dates[dateatt[0]].toordinal(),
+                                         dates[dateatt[1]].toordinal())]
+    else:
+        if args.start_date is not None and args.end_date is not None:
+            args.log.warning('Using "start_date" and "date_length", '
+                             'however, you specified "end_date" as well '
+                             'which will not be used.')
+            args.end_date = None
+        if args.start_date is not None:
+            base = dt(int(args.start_date[:4]),
+                      int(args.start_date[4:6]),
+                      int(args.start_date[6:]))
+            args.daterange = [base + datetime.timedelta(days=x)
+                              for x in range(0, args.date_length)]
+
+        if args.end_date is not None:
+            base = dt(int(args.end_date[:4]),
+                      int(args.end_date[4:6]),
+                      int(args.end_date[6:]))
+            args.daterange = [base - datetime.timedelta(days=x)
+                              for x in range(0, args.date_length)]
+
+    return args
